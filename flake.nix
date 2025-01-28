19b04f0f51f7d7e9dc9cb2e3954cd19c9424aaed
Stefan Piller <piller.stefan@pm.me>
2025-01-28 20:24:01 +0100

    added disko-config

--- flake.nix
+++ flake.nix
@@ -64,6 +64,8 @@
       #     ./nixos/configuration.nix
       #   ];
       # };
+      # export NIX_CONFIG="experimental-features = nix-command flakes"
+      # $ sudo nix run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake '.#nixosvm' --disk main /dev/vda
       # FIXME replace with your hostname
       nixosvm = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
