b6c1e909d35fe6ac884fc11c1ab441a1a9019015
Stefan Piller <piller.stefan@pm.me>
2025-02-01 13:35:59 +0100

    updated readme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -59,12 +59,13 @@
 
   console.keyMap = "de";
 
-
-  
-
   # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  programs.neovim.enable = true;
+  home.packages = with pkgs; [ 
+    git 
+    steam 
+    brave
+   ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
@@ -74,23 +75,23 @@
   systemd.user.startServices = "sd-switch";
 
 
-  # # Set your time zone.
-  # time.timeZone = "Europe/Vienna";
+  # Set your time zone.
+  time.timeZone = "Europe/Vienna";
 
-  # # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_AT.UTF-8";
 
-  # i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "de_AT.UTF-8";
-  #   LC_IDENTIFICATION = "de_AT.UTF-8";
-  #   LC_MEASUREMENT = "de_AT.UTF-8";
-  #   LC_MONETARY = "de_AT.UTF-8";
-  #   LC_NAME = "de_AT.UTF-8";
-  #   LC_NUMERIC = "de_AT.UTF-8";
-  #   LC_PAPER = "de_AT.UTF-8";
-  #   LC_TELEPHONE = "de_AT.UTF-8";
-  #   LC_TIME = "de_AT.UTF-8";
-  # };
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
