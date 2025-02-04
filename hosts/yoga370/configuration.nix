c84f1189fd9cc2dfa761507a2ddd84f2abd2ef5c
pillerstefan <piller.stefan@proton.me>
2025-02-04 14:52:21 +0100

    added de keyboard layout to yoga370

--- hosts/yoga370/configuration.nix
+++ hosts/yoga370/configuration.nix
@@ -35,6 +35,33 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+ services.xserver.xkb = {
+     layout ="de";
+    variant ="";
+
+  };
+
+  console.keyMap = "de";
+
+
+   # Set your time zone.
+   time.timeZone = "Europe/Vienna";
+
+   # Select internationalisation properties.
+   i18n.defaultLocale = "de_AT.UTF-8";
+
+   i18n.extraLocaleSettings = {
+     LC_ADDRESS = "de_AT.UTF-8";
+     LC_IDENTIFICATION = "de_AT.UTF-8";
+     LC_MEASUREMENT = "de_AT.UTF-8";
+     LC_MONETARY = "de_AT.UTF-8";
+     LC_NAME = "de_AT.UTF-8";
+     LC_NUMERIC = "de_AT.UTF-8";
+     LC_PAPER = "de_AT.UTF-8";
+     LC_TELEPHONE = "de_AT.UTF-8";
+     LC_TIME = "de_AT.UTF-8";
+   };
+
 
   nixpkgs = {
     # You can add overlays here
